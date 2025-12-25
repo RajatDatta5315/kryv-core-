eaff23677c62754b7f7bcd38c6e6220a993862e7
RajatDatta5315 <rajatdatta90000@gmail.com>
2025-12-25 18:31:44 +0530

    Create page.tsx

---
+++ app/terminal/page.tsx
@@ -0,0 +1,8 @@
+import NehiraTerminal from '@/components/NehiraTerminal';
+export default function TerminalPage() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center p-4">
+      <NehiraTerminal />
+    </div>
+  );
+}
